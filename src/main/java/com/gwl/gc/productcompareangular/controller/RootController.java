991a7fe7bc56c2ab60956a843db0cfec48907097
Chung, Jennifer (GC) <Jennifer.ChungGC@gwl.ca>
2018-06-21 11:52:31 -0400

    Fix all the indentations and remove any unused imports.

--- src/main/java/com/gwl/gc/productcompareangular/controller/RootController.java
+++ src/main/java/com/gwl/gc/productcompareangular/controller/RootController.java
@@ -1,17 +1,14 @@
 package com.gwl.gc.productcompareangular.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RootController {
 
-   @RequestMapping("/")
-   public String index() {
-      return "index";
-   }
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
 
 }
